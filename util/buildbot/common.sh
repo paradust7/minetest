2564c22bc8926c88169e6e6137817214cb06b87e
paradust7 <102263465+paradust7@users.noreply.github.com>
2024-02-14 02:11:09 -0800

    Fix slash

--- util/buildbot/common.sh
+++ util/buildbot/common.sh
@@ -46,7 +46,7 @@ get_sources () {
 }
 
 get_sources_irrlicht () {
-	local ref=`cat "$sourcedir/misc\irrlichtmt_tag.txt"`
+	local ref=`cat "$sourcedir/misc/irrlichtmt_tag.txt"`
 	pushd "$sourcedir"
 	rm -rf lib/irrlichmt
 	git clone https://github.com/paradust7/irrlicht lib/irrlichtmt --depth 1 -b "$ref"
